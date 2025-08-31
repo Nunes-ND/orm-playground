105525f0a08f7d12f9d7dee053efe8f882aa16a9
Gilson Nunes <GilsonNunes_dev@outlook.com>
2025-08-31 11:12:41 -0300

    refactor: adjust the location of the typeorm database

--- src/typeorm/data-source.ts
+++ src/typeorm/data-source.ts
@@ -2,7 +2,7 @@ import { DataSource, type DataSourceOptions } from 'typeorm';
 
 const dataSourceOptions: DataSourceOptions = {
 	type: 'better-sqlite3',
-	database: `${__dirname}/database.sqlite`,
+	database: String(process.env.DB_FILE),
 	entities: [`${__dirname}/entities/*.ts`],
 	migrations: [`${__dirname}/migrations/*.ts`],
 	subscribers: [`${__dirname}/subscribers/*.ts`],
